f6e36d996425da09f8581bd3a579dd6aee221575
Thomas Schwabauer <thomas.schwabauer@gmx.net>
2024-08-06 16:07:41 +0100

    update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import Project from './Project';
+import React from "react";
 import { projects } from "../utils/projects"; // Import the Project component
+import Project from "./Project";
 
 const Portfolio: React.FC = () => {
-    return (
-        <div className="pt-20 h-screen flex flex-wrap items-center justify-center container mx-auto mt-8" id="projects">
-            <h2 className="text-4xl font-bold text-white-900">My Projects</h2>
-            <div className="flex flex-wrap">
-                {projects.map((project, index) => (
-                    <Project
-                        key={index}
-                        image={project.image}
-                        description={project.description}
-                        technologies={project.technologies}
-                        githubLink={project.githubLink}
-                        livePreviewLink={project.livePreviewLink}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="pt-20 h-fit flex flex-wrap items-center justify-center container mx-auto mt-8"
+      id="projects"
+    >
+      <h2 className="text-4xl font-bold text-white-900">My Projects</h2>
+      <div className="flex flex-wrap">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            image={project.image}
+            description={project.description}
+            technologies={project.technologies}
+            githubLink={project.githubLink}
+            livePreviewLink={project.livePreviewLink}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,20 +1,41 @@
 import AmselLogo from "../assets/amsel_logo.jpg";
 import MyDexLogo from "../assets/mydex.png";
+import PerionDashboard from "../assets/perion_dashboard.png";
+import Portfolio from "../assets/portfolio.png";
 
 export const projects = [
-    {
-        id: 1,
-        image: AmselLogo,
-        description: 'A hybrid Webapp for searching/buying farmer and vending machine products.',
-        technologies: 'Flutter, Firebase',
-        livePreviewLink: 'https://amsel-ths.web.app/#/',
-    },
-    {
-        id: 2,
-        image: MyDexLogo,
-        description: 'A full decentralized Exchange deployed on the Sepolia Testnet, including the solidity smart contracts.',
-        technologies: 'React, Solidity, Hardhat, Foundry',
-        githubLink: 'https://github.com/tjoooobooo/web3-academy',
-        livePreviewLink: 'https://my-o2wznex4e-tjoooobooo.vercel.app/',
-    }
+  {
+    id: 1,
+    image: AmselLogo,
+    description:
+      "A hybrid Webapp for searching/buying farmer and vending machine products.",
+    technologies: "Flutter, Firebase",
+    livePreviewLink: "https://amsel-ths.web.app/#/",
+  },
+  {
+    id: 2,
+    image: MyDexLogo,
+    description:
+      "A full decentralized Exchange deployed on the Sepolia Testnet, including the solidity smart contracts.",
+    technologies: "React, Solidity, Hardhat, Foundry",
+    githubLink: "https://github.com/tjoooobooo/web3-academy",
+    livePreviewLink: "https://my-o2wznex4e-tjoooobooo.vercel.app/",
+  },
+  {
+    id: 3,
+    image: PerionDashboard,
+    description:
+      "A dashboard portfolio which grabs daily data for crypto prices and other relevant web3 meta data",
+    technologies: "React, TypeScript, API's, Cron",
+    // githubLink: 'https://github.com/tjoooobooo/perion_dashboard',
+    livePreviewLink: "https://perion-dashboard.vercel.app/",
+  },
+  {
+    id: 4,
+    image: Portfolio,
+    description: "My portfolio website",
+    technologies: "Vue.js, TypeScript, Tailwind",
+    githubLink: "https://github.com/tjoooobooo/my-profile",
+    livePreviewLink: "https://www.thomas-schwabauer.de/",
+  },
 ];
