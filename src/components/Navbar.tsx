4804e9a31fd4f21b974f8cffd7d7072ddcb21596
Thomas Schwabauer <thomas.schwabauer@gmx.net>
2024-08-06 21:16:26 +0100

    close navbar on outside click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HashLink as NavHashLink } from "react-router-hash-link";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="fixed top-0 left-0 w-full z-10 bg-white shadow">
@@ -59,43 +73,46 @@ export default function Navbar() {
 
         {/* Mobile menu */}
         <div
+          ref={menuRef}
           className={`fixed top-0 right-0 h-screen w-8/12 bg-white border transition-all duration-300 ${
             isMenuOpen
               ? "translate-x-0 opacity-100"
               : "translate-x-full opacity-0"
           }`}
         >
-          <div className="flex flex-col text-start w-full text-base cursor-pointer pt-10">
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#home" onClick={toggleMenu}>
-                Home
-              </NavHashLink>
-            </li>
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#about" onClick={toggleMenu}>
-                About me
-              </NavHashLink>
-            </li>
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#work" onClick={toggleMenu}>
-                Work
-              </NavHashLink>
-            </li>
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#projects" onClick={toggleMenu}>
-                Projects
-              </NavHashLink>
-            </li>
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#documents" onClick={toggleMenu}>
-                Documents
-              </NavHashLink>
-            </li>
-            <li className="hover:bg-gray-200 py-4 px-6 w-full">
-              <NavHashLink smooth to="/#contact" onClick={toggleMenu}>
-                Contact
-              </NavHashLink>
-            </li>
+          <div className="flex flex-col w-full text-base cursor-pointer pt-10 text-left">
+            <ul className="list-none p-0 m-0">
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#home" onClick={toggleMenu}>
+                  Home
+                </NavHashLink>
+              </li>
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#about" onClick={toggleMenu}>
+                  About me
+                </NavHashLink>
+              </li>
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#work" onClick={toggleMenu}>
+                  Work
+                </NavHashLink>
+              </li>
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#projects" onClick={toggleMenu}>
+                  Projects
+                </NavHashLink>
+              </li>
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#documents" onClick={toggleMenu}>
+                  Documents
+                </NavHashLink>
+              </li>
+              <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                <NavHashLink smooth to="/#contact" onClick={toggleMenu}>
+                  Contact
+                </NavHashLink>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
