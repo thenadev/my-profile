99e4011a56214ae0595f178f72591d7d2de9654e
thomas <thomas.schwabauer@gmx.net>
2024-09-30 17:06:39 +0200

    add speedinsights

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/home" element={<Home/>}/>
-                    <Route path="/blog" element={<Blog/>}/>
-                    <Route path="*" element={<NoPage/>}/>
-                </Routes>
-            </BrowserRouter>
-            <SpeedInsights />
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+      <SpeedInsights />
+    </div>
+  );
 }
 
 export default App;
